538f89b86bc549385a27063846a82a866d402efd
MemmedovaEsmira <memmedovaesmira151@gmail.com>
2024-01-23 02:48:18 +0400

    task added

--- first_project/src/App.jsx
+++ first_project/src/App.jsx
@@ -1,16 +1,60 @@
 import Card from './components/Card/Card';
 
 
-function App() {
+// function App() {
 
+//   return (
+//     <div>
+//     <h2 className='APP'>Suggested for you</h2>
+// <Card/>
+// <Card/>
+// <Card/>
+//   </div>
+//   )
+// }
+
+// export default App;
+
+
+
+function Main() {
+  const card =
+  [
+    {
+      views:129.758,
+      price: 75.8,
+      score:4.2,
+       id: 1,
+    },
+    {
+      views:110.7,
+      price: 60,
+      score:3,
+       id: 1,
+    },
+    {
+      views:105.85,
+      price: 40,
+      score:2.5,
+       id: 1,
+    },
+  
+  ]
   return (
-    <div>
-    <h2 className='APP'>Suggested for you</h2>
-<Card/>
-<Card/>
-<Card/>
-  </div>
-  )
-}
-
-export default App;
+    <>
+   
+     {card.map((item)=>{
+     return <Card 
+     views={item.views} 
+      price={item.price} 
+      score={item.score}
+      key={item.id} 
+      />
+      
+    })}
+      </>
+    );
+  }
+  
+  
+  export default Main;
--- first_project/src/components/Card/Card.jsx
+++ first_project/src/components/Card/Card.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import './card.css'
-// import card_img from "../Image/img.jpg";
+import card_img from "../../assets/Image/img.jpg";
+import { MdVisibility } from "react-icons/md";
+import { FaStar } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
+import { FiShoppingBag } from "react-icons/fi";
+
 
 const Card = (props) => {
   return (
     <>
     <div className='cards'>
     <div className="card">
-     {/* <img src={img} alt="" /> */}
+    <img src={card_img} alt="image" />
       <h4 className='card_title'>Phyton for Data Science and Machine Learning Bootcamp</h4>
       <div className='card_view'>
-      <span>Jose spanortilla</span>    
-      <h3>129,758</h3>
+      <span>Jose Portilla</span> 
+      <h3>
+      <MdVisibility />  
+      {props.views} 
+        </h3>
       </div>
       <div className='card_view'>
-       <h3>4.2</h3>
+       <h3> {props.score}
+       <FaStar /><FaStar /><FaStar /><FaStar /><FaRegStar />
+       </h3>
        <button className='btn'>Most popular</button>
       </div>
       <div className='card_view'>
-      <button className='card_button'>Add too cart</button>
-      <h3>75,80$</h3>
+      <button className='card_button'>Add too cart <FiShoppingBag /></button>
+     <h3>$ {props.price}
+     </h3>
       </div>
     </div>
     
